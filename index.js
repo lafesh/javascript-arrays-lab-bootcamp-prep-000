7f262402a0a4687140a3c2c234fa8f5b232ad122
Gina Fedder <ginafedder@yahoo.com>
2018-08-27 20:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
    return [...kittens, "Broom"];
 }
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+
+
